3e2d30d804a87b9f2a9a0a6cf404d9587f2f95fc
Anthony T. <antonis301@gmail.com>
2024-06-07 20:57:41 +0300

    Hidden clear work dir button

--- src/main/java/org/kronos/SettingsUI.java
+++ src/main/java/org/kronos/SettingsUI.java
@@ -24,6 +24,7 @@ public class SettingsUI extends JPanel {
     public SettingsUI() {
         initComponents();
         loadSettings();
+        clearWorkDirectory.setVisible(false); // hidden for now
     }
 
     void loadSettings() {
