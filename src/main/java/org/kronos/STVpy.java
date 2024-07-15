b5339d2624973b2601df664e1d69ffea4026552a
Anthony T. <antonis301@gmail.com>
2024-07-15 18:54:36 +0300

    Added constituency file in STVpy class

--- src/main/java/org/kronos/STVpy.java
+++ src/main/java/org/kronos/STVpy.java
@@ -7,16 +7,16 @@ public class STVpy {
         CallPython py = new CallPython(coreScript, "-b", ballotFile, "-l", "DEBUG", "-s", Integer.toString(seats));
         return py.run();
     }
-    public String callSTV(String ballotFile, int seats, String randomSeed) throws Exception {
-        CallPython py = new CallPython(coreScript, "-b", ballotFile, "-l", "DEBUG", "-s", Integer.toString(seats), "-r", randomSeed);
-        return py.run();
-    }
     public String callSTV(String ballotFile) throws Exception {
         CallPython py = new CallPython(coreScript, "-b", ballotFile, "-l", "DEBUG");
         return py.run();
     }
     public String callSTV(String ballotFile, String constituencyFile) throws Exception {
         CallPython py = new CallPython(coreScript, "-b", ballotFile, "-l", "DEBUG", "-c", constituencyFile);
+        return py.run();
+    }
+    public String callSTV(String ballotFile, int seats, String constituencyFile) throws Exception {
+        CallPython py = new CallPython(coreScript, "-b", ballotFile, "-l", "DEBUG", "-s", Integer.toString(seats), "-c", constituencyFile);
         return py.run();
     }
 }
