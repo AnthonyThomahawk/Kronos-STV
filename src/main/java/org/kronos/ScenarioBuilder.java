3a4aec8e8b4932dc15ff11278ac15416dd084fc2
Anthony T. <antonis301@gmail.com>
2024-12-13 21:09:08 +0200

    Added scenario build functionality and integrated scenario generator lib

--- src/main/java/org/kronos/ScenarioBuilder.java
+++ src/main/java/org/kronos/ScenarioBuilder.java
@@ -7,22 +7,126 @@ package org.kronos;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.GroupLayout;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableColumn;
-import java.awt.*;
-import java.util.EventObject;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Worker
  */
 public class ScenarioBuilder extends JPanel {
+    ArrayList<ArrayList<JComboBox>> comboBoxGroups;
+    ArrayList<String> options;
+    ArrayList<String> patterns;
 
     public ScenarioBuilder() {
         initComponents();
+
+        options = new ArrayList<>();
+        options.add("Test 1");
+        options.add("Test 2");
+        options.add("Test 3");
+        options.add("Test 4");
+
         init();
+    }
+
+    private ArrayList<String> getExRand() {
+        DefaultTableModel dtm = (DefaultTableModel) exRandtable.getModel();
+        ArrayList<String> exRandList = new ArrayList<>();
+
+        for (int i = 0; i < dtm.getRowCount(); i++) {
+            if ((Boolean) dtm.getValueAt(i, 1)) {
+                exRandList.add((String) dtm.getValueAt(i, 0));
+            }
+        }
+
+        return exRandList;
+    }
+
+    // a better rand for the other lib (keep for later)
+//    private String getExRand(ArrayList<String> exclude) {
+//        DefaultTableModel dtm = (DefaultTableModel) exRandtable.getModel();
+//
+//        ArrayList<String> exRandList = new ArrayList<>();
+//
+//        for (int i = 0; i < dtm.getRowCount(); i++) {
+//            if ((Boolean) dtm.getValueAt(i, 1)) {
+//                exRandList.add((String) dtm.getValueAt(i, 0));
+//            }
+//        }
+//
+//        exRandList.removeAll(exclude);
+//
+//        Collections.shuffle(exRandList);
+//
+//        return exRandList.get(0);
+//    }
+//
+//    private String getRand(ArrayList<String> exclude) {
+//        ArrayList<String> rem = new ArrayList<>(options);
+//        rem.removeAll(exclude);
+//
+//        Collections.shuffle(rem);
+//        return rem.get(0);
+//    }
+
+    private ArrayList<JComboBox> createComboBoxGroup(ArrayList<String> opts) {
+        ArrayList<JComboBox> arr = new ArrayList<>();
+        for (int i = 0; i < permTable.getColumnCount() - 1; i++) {
+            JComboBox x = new JComboBox();
+            for (String s : opts) {
+                x.addItem(s);
+            }
+            x.addItem("RANDOM");
+            x.addItem("EX-RANDOM");
+            x.addItem("Clear option [x]");
+            x.setSelectedIndex(-1);
+
+            AtomicInteger oldSel = new AtomicInteger(-1);
+
+            x.addItemListener(e -> {
+                if (x.getSelectedIndex() == -1)
+                    return;
+
+                if (x.getSelectedIndex() == x.getItemCount() - 1) {
+                    x.setSelectedIndex(-1);
+                    oldSel.set(-1);
+                    return;
+                }
+
+                int myInd = arr.indexOf(x);
+
+                for (int j = 0; j < arr.size(); j++) {
+                    if (j == myInd) // ignore self
+                        continue;
+                    if (x.getSelectedIndex() >= options.size()) // ignore random, ex-random
+                        continue;
+                    if (arr.get(j).getSelectedIndex() == x.getSelectedIndex()) {
+                        DefaultTableModel dtm = (DefaultTableModel) permTable.getModel();
+
+                        // swap elements
+                        int newSel = x.getSelectedIndex();
+                        if (oldSel.get() == newSel) {
+                            arr.get(j).setSelectedIndex(-1);
+                        } else {
+                            arr.get(j).setSelectedIndex(oldSel.get());
+                        }
+                        int ind = permTable.getSelectedRow();
+                        dtm.setValueAt(arr.get(j).getSelectedItem(), ind, j+1);
+                    }
+                }
+
+                oldSel.set(x.getSelectedIndex());
+            });
+
+            arr.add(x);
+        }
+
+        return arr;
     }
 
     private void init() {
@@ -45,10 +149,25 @@ public class ScenarioBuilder extends JPanel {
 
         dtm.addColumn("Candidate");
         dtm.addColumn("Check");
-        dtm.addRow(new Object[]{"Test 1", Boolean.TRUE});
-        dtm.addRow(new Object[]{"Test 2", Boolean.FALSE});
-        dtm.addRow(new Object[]{"Test 3", Boolean.FALSE});
-        dtm.addRow(new Object[]{"Test 4", Boolean.FALSE});
+        for (String s : options) {
+            dtm.addRow(new Object[]{s, Boolean.FALSE});
+        }
+
+        String multInfo = "<html>Multiplier values : <br>" +
+                "<b>Any number</b> - Number of votes<br>" +
+                "<b> ? </b> - Remaining votes";
+
+        label4.setText(multInfo);
+
+        permTable = new JTable() {
+            @Override
+            public TableCellEditor getCellEditor(int row, int column) {
+                if (column != 0) {
+                    return new DefaultCellEditor(comboBoxGroups.get(row).get(column-1));
+                }
+                return super.getCellEditor(row, column);
+            }
+        };
 
         DefaultTableModel dtm2 = (DefaultTableModel) permTable.getModel();
 
@@ -57,12 +176,6 @@ public class ScenarioBuilder extends JPanel {
         for (int i = 0; i < 4; i++) {
             dtm2.addColumn("Choice " + i);
         }
-
-        String multInfo = "<html>Multiplier values : <br>" +
-                "<b>Any number</b> - Number of votes<br>" +
-                "<b> ? </b> - Remaining votes";
-
-        label4.setText(multInfo);
 
         permTable.setShowVerticalLines(true);
         permTable.setShowHorizontalLines(true);
@@ -75,11 +188,74 @@ public class ScenarioBuilder extends JPanel {
         exRandtable.setColumnSelectionAllowed(false);
         exRandtable.setRowSelectionAllowed(true);
         exRandtable.getTableHeader().setReorderingAllowed(false);
+
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+
+        for (int i = 0; i < permTable.getColumnCount(); i++) {
+            permTable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+        }
+
+        scrollPane2.setViewportView(permTable);
+
+        comboBoxGroups = new ArrayList<>();
     }
 
     private void addBtn(ActionEvent e) {
         DefaultTableModel dtm2 = (DefaultTableModel) permTable.getModel();
+        comboBoxGroups.add(createComboBoxGroup(options));
         dtm2.addRow(new Object[] {0, null, null, null, null});
+    }
+
+    private void remBtn(ActionEvent e) {
+        int[] rows = permTable.getSelectedRows();
+        DefaultTableModel dtm2 = (DefaultTableModel) permTable.getModel();
+        for (int r : rows) {
+            comboBoxGroups.remove(r);
+            dtm2.removeRow(r);
+        }
+    }
+
+    private void buildBtn(ActionEvent e) {
+        patterns = new ArrayList<>();
+
+        patterns.add(Integer.toString((Integer) spinner1.getValue()));
+        ArrayList<String> exRand = getExRand();
+        StringBuilder exRandStr = new StringBuilder("#={" + exRand.get(0));
+        for (int i = 1; i < exRand.size(); i++) {
+            exRandStr.append(",").append(exRand.get(i));
+        }
+        exRandStr.append("}");
+        patterns.add(exRandStr.toString());
+
+        DefaultTableModel dtm = (DefaultTableModel) permTable.getModel();
+
+        for (int i = 0; i < dtm.getRowCount(); i++) {
+            String first;
+
+            if (dtm.getValueAt(i, 1).equals("EX-RANDOM"))
+                first = "#";
+            else if (dtm.getValueAt(i, 1).equals("RANDOM"))
+                first = "$";
+            else
+                first = (String) dtm.getValueAt(i, 1);
+
+            StringBuilder line = new StringBuilder(dtm.getValueAt(i, 0) + "*" + first);
+            for (int j = 2; j < dtm.getColumnCount(); j++) {
+                if (dtm.getValueAt(i,j) != null)
+                    if (dtm.getValueAt(i,j).equals("EX-RANDOM"))
+                        line.append("|").append("#");
+                    else if (dtm.getValueAt(i,j).equals("RANDOM"))
+                        line.append("|").append("$");
+                    else
+                        line.append("|").append(dtm.getValueAt(i,j));
+
+            }
+            patterns.add(line.toString());
+        }
+
+        ScenarioGenerator sg = new ScenarioGenerator(options, patterns, (Integer)spinner2.getValue());
+        sg.ballotsToCSV("b.csv");
     }
 
     private void initComponents() {
@@ -97,6 +273,9 @@ public class ScenarioBuilder extends JPanel {
         addBtn = new JButton();
         remBtn = new JButton();
         label4 = new JLabel();
+        buildBtn = new JButton();
+        label5 = new JLabel();
+        spinner2 = new JSpinner();
 
         //======== this ========
 
@@ -125,9 +304,17 @@ public class ScenarioBuilder extends JPanel {
 
         //---- remBtn ----
         remBtn.setText("Remove -");
+        remBtn.addActionListener(e -> remBtn(e));
 
         //---- label4 ----
         label4.setText("Multiplier possible values :");
+
+        //---- buildBtn ----
+        buildBtn.setText("Build");
+        buildBtn.addActionListener(e -> buildBtn(e));
+
+        //---- label5 ----
+        label5.setText("Seats");
 
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
@@ -135,10 +322,16 @@ public class ScenarioBuilder extends JPanel {
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
-                    .addGroup(layout.createParallelGroup()
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(addBtn)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(remBtn)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(buildBtn))
                         .addComponent(scrollPane2, GroupLayout.PREFERRED_SIZE, 441, GroupLayout.PREFERRED_SIZE)
                         .addGroup(layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup()
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                                 .addGroup(layout.createSequentialGroup()
                                     .addGroup(layout.createParallelGroup()
                                         .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 187, GroupLayout.PREFERRED_SIZE)
@@ -147,15 +340,16 @@ public class ScenarioBuilder extends JPanel {
                                     .addGroup(layout.createParallelGroup()
                                         .addComponent(spinner1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                         .addComponent(label2)))
-                                .addComponent(label4))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(label4)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addGroup(layout.createParallelGroup()
+                                        .addComponent(label5)
+                                        .addComponent(spinner2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
                             .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                             .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                                 .addComponent(label3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE)))
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(addBtn)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(remBtn)))
+                                .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE))))
                     .addContainerGap(8, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -173,14 +367,20 @@ public class ScenarioBuilder extends JPanel {
                                 .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                 .addComponent(spinner1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                             .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(label4))
+                            .addGroup(layout.createParallelGroup()
+                                .addComponent(label4)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(label5)
+                                    .addGap(6, 6, 6)
+                                    .addComponent(spinner2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
                         .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE))
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
                     .addComponent(scrollPane2, GroupLayout.PREFERRED_SIZE, 312, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(addBtn)
-                        .addComponent(remBtn))
+                        .addComponent(remBtn)
+                        .addComponent(buildBtn))
                     .addContainerGap())
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
@@ -200,5 +400,8 @@ public class ScenarioBuilder extends JPanel {
     private JButton addBtn;
     private JButton remBtn;
     private JLabel label4;
+    private JButton buildBtn;
+    private JLabel label5;
+    private JSpinner spinner2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
---
+++ src/main/java/org/kronos/ScenarioGenerator.java
@@ -0,0 +1,159 @@
+package org.kronos;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class ScenarioGenerator {
+    ArrayList<String> candidates;
+    ArrayList<String[]> ballots;
+    ArrayList<String> candidatesSubset;
+    int ballotCount;
+    int seats;
+
+    public ScenarioGenerator(String candidatesFile, String patternsFile, int seats) {
+        readCandidatesFile(candidatesFile);
+        readPatternFile(patternsFile);
+        this.seats = seats;
+    }
+
+    public ScenarioGenerator(ArrayList<String> candidates, ArrayList<String> patterns, int seats) {
+        this.candidates = new ArrayList<>(candidates);
+        this.seats = seats;
+        ballots = new ArrayList<>();
+        patterns.forEach(this::processLine);
+    }
+
+    void processCandidateLine(String line) {
+        if (!line.isEmpty())
+            candidates.add(line.trim());
+    }
+
+    public void readCandidatesFile(String fileName) {
+        candidates = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(this::processCandidateLine);
+        } catch (Exception e) {
+            System.out.println("Failed to read file " + e);
+        }
+    }
+
+    public String[] makeBallot(String pattern) {
+        String[] patternTokens = pattern.split("\\|");
+        String[] preparedBallot = new String[patternTokens.length];
+
+        ArrayList<String> remainingCandidates = new ArrayList<>(candidates);
+        ArrayList<String> localCandidateSubSet = null;
+
+        if (candidatesSubset != null)
+            localCandidateSubSet = new ArrayList<>(candidatesSubset);
+
+        for (int i = 0; i < patternTokens.length; i++) {
+            if (candidates.contains(patternTokens[i])) {
+                if (!remainingCandidates.contains(patternTokens[i])) {
+                    throw new NumberFormatException("Duplicate candidate in ballot!");
+                }
+
+                preparedBallot[i] = patternTokens[i];
+                remainingCandidates.remove(preparedBallot[i]);
+            } else if (patternTokens[i].equals("$")) {
+                if (remainingCandidates.isEmpty())
+                    throw new NumberFormatException("There are no more candidates in random set.");
+
+                Random ra = new Random();
+                preparedBallot[i] = remainingCandidates.get(ra.nextInt(remainingCandidates.size()));
+                remainingCandidates.remove(preparedBallot[i]);
+            } else if (patternTokens[i].equals("#") && localCandidateSubSet != null) {
+                for (String c : candidates) {
+                    if (!remainingCandidates.contains(c)) {
+                        localCandidateSubSet.remove(c);
+                    }
+                }
+
+                if (localCandidateSubSet.isEmpty())
+                    throw new NumberFormatException("There are no more candidates in exclusive random set.");
+
+                Random ra = new Random();
+                preparedBallot[i] = localCandidateSubSet.get(ra.nextInt(localCandidateSubSet.size()));
+                remainingCandidates.remove(preparedBallot[i]);
+                localCandidateSubSet.remove(preparedBallot[i]);
+            }
+            else {
+                throw new NumberFormatException("Value : " + patternTokens[i] + " is not an index nor a bound letter (R or r)");
+            }
+        }
+
+        return preparedBallot;
+    }
+
+    public void ballotsToCSV(String fileName) {
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+            PrintWriter writer = new PrintWriter(osw);
+
+            for (String[] b : ballots) {
+                StringBuilder line = new StringBuilder();
+                for (int i = 0; i < b.length; i++) {
+                    line.append(b[i]);
+                    if (i != b.length-1)
+                        line.append(",");
+                }
+                writer.println(line);
+            }
+
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Failed writing file " + e);
+        }
+    }
+
+    void processLine(String line) {
+        if (line.matches("[0-9]+")){
+            ballotCount = Integer.parseInt(line);
+        } else if (line.contains("#=")) {
+            candidatesSubset = new ArrayList<>();
+            String subSetStr = line.split("#=\\{")[1].split("}")[0];
+            String[] elements = subSetStr.split(",");
+
+            candidatesSubset.addAll(Arrays.asList(elements));
+        }
+        else {
+            String[] split1 = line.split("\\*");
+            int multiplier;
+            if (split1[0].trim().equals("?")) {
+                multiplier = ballotCount - ballots.size();
+            } else {
+                multiplier = Integer.parseInt(split1[0].trim());
+            }
+
+            if (multiplier <= 0) {
+                throw new NumberFormatException("Multiplier is negative (no remaining votes)");
+            }
+
+            String pattern = split1[1].trim();
+
+            for (int i = 0 ; i < multiplier; i++) {
+                ballots.add(makeBallot(pattern));
+            }
+        }
+    }
+
+    public void readPatternFile(String fileName) {
+        ballots = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(this::processLine);
+        } catch (Exception e) {
+            System.out.println("Failed reading file " + e);
+            System.exit(-1);
+        }
+    }
+}
